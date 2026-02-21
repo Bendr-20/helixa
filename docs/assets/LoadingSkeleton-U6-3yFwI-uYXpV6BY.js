5cd9be8c18d17c0b1a55f655ccec9f9173178dd3
Bendr 2.0 <bendr2@helixa.xyz>
2026-02-21 03:12:06 +0000

    Link manage edit button to manage.html with preloaded data

---
+++ docs/assets/LoadingSkeleton-U6-3yFwI-uYXpV6BY.js
@@ -0,0 +1,17 @@
+import{aW as r,be as a}from"./index-De1e-9qj.js";let i=a`
+  from, to {
+    background: var(--privy-color-foreground-4);
+    color: var(--privy-color-foreground-4);
+  }
+
+  50% {
+    background: var(--privy-color-foreground-accent);
+    color: var(--privy-color-foreground-accent);
+  }
+`;const c=r`
+  ${o=>o.$isLoading?r`
+          width: 35%;
+          animation: ${i} 2s linear infinite;
+          border-radius: var(--privy-border-radius-sm);
+        `:""}
+`;export{c as n};
