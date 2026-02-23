9bc6c74679be76f03629c76d7fcb7a360c3a69b0
Bendr 2.0 <bendr2@helixa.xyz>
2026-02-23 21:59:25 +0000

    fix: clean rebuild - Manage page SPA (no redirect)

---
+++ docs/assets/LoadingSkeleton-U6-3yFwI-CvnulwFW.js
@@ -0,0 +1,17 @@
+import{aT as r,bb as a}from"./index-D-8GrUwK.js";let i=a`
+  from, to {
+    background: var(--privy-color-foreground-4);
+    color: var(--privy-color-foreground-4);
+  }
+
+  50% {
+    background: var(--privy-color-foreground-accent);
+    color: var(--privy-color-foreground-accent);
+  }
+`;const c=r`
+  ${o=>o.$isLoading?r`
+          width: 35%;
+          animation: ${i} 2s linear infinite;
+          border-radius: var(--privy-border-radius-sm);
+        `:""}
+`;export{c as n};
