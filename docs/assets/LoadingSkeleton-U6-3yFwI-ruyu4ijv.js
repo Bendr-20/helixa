e1b78c361185c3e15746aa016df1eb95913f80e1
Bendr 2.0 <bendr2@helixa.xyz>
2026-03-01 21:33:50 +0000

    rebuild: fresh build with updated Stake page

---
+++ docs/assets/LoadingSkeleton-U6-3yFwI-ruyu4ijv.js
@@ -0,0 +1,17 @@
+import{aR as r,b9 as a}from"./index-kqRS9694.js";let i=a`
+  from, to {
+    background: var(--privy-color-foreground-4);
+    color: var(--privy-color-foreground-4);
+  }
+
+  50% {
+    background: var(--privy-color-foreground-accent);
+    color: var(--privy-color-foreground-accent);
+  }
+`;const c=r`
+  ${o=>o.$isLoading?r`
+          width: 35%;
+          animation: ${i} 2s linear infinite;
+          border-radius: var(--privy-border-radius-sm);
+        `:""}
+`;export{c as n};
