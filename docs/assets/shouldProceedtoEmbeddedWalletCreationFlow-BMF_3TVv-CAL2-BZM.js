f0a34e112fe84c307129c9d4a03570d40322351c
Bendr 2.0 <bendr2@helixa.xyz>
2026-03-16 18:48:15 +0000

    fix: skip tsc for build, rebuild trust graph with glow bubbles

--- docs/assets/shouldProceedtoEmbeddedWalletCreationFlow-BMF_3TVv-CAL2-BZM.js
+++
@@ -1,1 +0,0 @@
-import{aE as o,aF as r}from"./index-Cev-sTME.js";const i=(a,e)=>o(a,e.ethereum.createOnLogin)||r(a,e.solana.createOnLogin);export{i as a};
---
+++ docs/assets/shouldProceedtoEmbeddedWalletCreationFlow-BMF_3TVv-UJECnaro.js
@@ -0,0 +1,1 @@
+import{aE as o,aF as r}from"./index-BnvnNUhS.js";const i=(a,e)=>o(a,e.ethereum.createOnLogin)||r(a,e.solana.createOnLogin);export{i as a};
